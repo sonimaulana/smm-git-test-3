4b09d5ca736c2e569e94c9b894424d2a79c009a7
sonimaulana <sonimaulanam11@gmail.com>
2023-09-16 10:17:48 +0700

    Menambahkan ul li

--- index.php
+++ index.php
@@ -8,5 +8,9 @@
     <p>
         Lorem ipsum dolor sit amet consectetur adipisicing elit. Magni expedita sint cupiditate? Asperiores, soluta! Enim animi sint consequatur praesentium temporibus quisquam aut nisi dignissimos, optio libero veritatis. Beatae, quibusdam harum?
     </p>
+    <ul>
+        <li>Soni Maulana</li>
+        <li>Nurul Hisan</li>
+    </ul>
 </body>
 </html>
