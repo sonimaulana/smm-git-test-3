eb3f7085fa2f6e77f710febbd771d302a8585dd4
sonimaulana <sonimaulanam11@gmail.com>
2023-09-16 10:08:19 +0700

    Menambahkan File inde.php

---
+++ index.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <title>Latihan GIT</title>
+</head>
+<body>
+    <h1>Hello World</h1>
+    <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Magni expedita sint cupiditate? Asperiores, soluta! Enim animi sint consequatur praesentium temporibus quisquam aut nisi dignissimos, optio libero veritatis. Beatae, quibusdam harum?
+    </p>
+</body>
+</html>
